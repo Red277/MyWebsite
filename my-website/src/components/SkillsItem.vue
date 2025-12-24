b9c2a47742c5f44999122f9c7cbdca12440634ae
Red277 <47482394+Red277@users.noreply.github.com>
2025-12-23 23:14:43 -0500

    adding skill view and item component

---
+++ my-website/src/components/SkillsItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-sheet
+    width="10%"
+    height="20%"
+    rounded
+    elevation="8"
+    class="pa-4"
+  >
+    <div style="height: 80%;">
+      <v-img
+        :src="props.urlRef"
+        alt="Skills Icon"
+        contain
+        height="100%"
+        width="100%"
+      ></v-img>
+    </div>
+    <div style="text-align: center; padding-top: 4px; font-weight: bold;">{{ props.skillTitle }} </div>
+
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    urlRef: String,
+    skillTitle: String
+  });
+</script>
+
+<style scoped>
+</style>
--- my-website/src/views/SkillsView.vue
+++ my-website/src/views/SkillsView.vue
@@ -1,9 +1,20 @@
-<template>
-  <div>
-    <h1>Skills</h1>
-    <p>My skills.</p>
-  </div>
-</template>
+<script setup lang="ts">
+import SkillsItem from '@/components/SkillsItem.vue';
 
-<style scoped>
-</style>
+</script>
+
+<template>
+    <div style="display: flex; justify-content: center; align-items: center; height: inherit; position: relative;">
+    <p style="text-align: left; font-size:2rem; top: 100px; position: absolute ;">Skills</p>
+    
+      <v-sheet
+        width="60%"
+        height="60%"
+        rounded
+        class="position-"
+      >
+        <SkillsItem skill-title="JavaScript" url-ref="/src/assets/javascript.svg"/>
+
+      </v-sheet>
+    </div>
+</template>
