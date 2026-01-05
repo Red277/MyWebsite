93ee84d17ed0dd403c9de0a0a693c5b9a452260c
Red277 <47482394+Red277@users.noreply.github.com>
2026-01-04 20:09:21 -0500

    updating homeview design

--- my-website/src/views/HomeView.vue
+++ my-website/src/views/HomeView.vue
@@ -1,10 +1,43 @@
-<script setup>
-const emit = defineEmits(['activate-navbar'])
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
 </script>
 <template>
+
+
+
   <div id="home" style="height: 100vh;">
-     <v-hover v-slot="{ isHovering, props }">
-          <v-btn @click="emit('activate-navbar')" v-bind="props" :color="isHovering ? 'orange' : undefined">Test</v-btn>
-      </v-hover>
+      <div style="justify-content: center; align-items: center; display: flex; flex-direction: column; height: 90%;">
+        
+
+        <p class="title">Hi, I’m<span class="highlight"> William</span></p> <br>
+        <p class="title">a <span class="highlight"> Full Stack Developer</span></p>
+
+
+      
+        <div style="position: relative; top: 20px;">
+          <v-hover v-slot="{ isHovering, props }">
+            <RouterLink to="/#about" active-class="">
+              <v-btn v-bind="props" :color="isHovering ? 'orange' : undefined">VIEW MY WORK</v-btn>
+            </RouterLink>
+          </v-hover>
+        </div>
+      </div>
+
   </div>
 </template>
+
+<style>
+  .title {
+    font-weight: 900;
+    font-size: 48px;
+    color: rgb(248, 233, 210);
+  }
+
+  .highlight{
+    font-weight: inherit;
+    font-size: inherit;
+    color: rgb(218, 141, 0);
+  }
+
+</style>
